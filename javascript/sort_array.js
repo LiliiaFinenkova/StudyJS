bc73052112b43b9e8b05c70dd82f436d41bdff53
liliia.finenkova@gmail.com <liliia.finenkova@gmail.com>
2017-09-20 20:09:18 +0300

    final working sorting function

--- javascript/sort_array.js
+++ javascript/sort_array.js
@@ -1,41 +1,70 @@
-//1.Bubble sort
-var sortBubblesArray = function(arr, asc) {
-    var result = [];
-    var cur;
-    var prev;
-    var iterationCount = 0;
-    var isCompleted;
+window.onload = function () {
+    // Generating the array
+    var displayArray;
+    var generatedArray;
+    var unsortedArray;
+debugger;
+    // 4. Display an unsorted array
+    document.querySelector('form').addEventListener('submit', function (e) {
+        // 1. Get amount of numbers
+debugger;
+        var numbersAmount = document.querySelector('#select_amount').value;
+        // 2. Get min and max values
+        var minValue = parseInt(document.querySelector('#min_value').value);
+        var maxValue = parseInt(document.querySelector('#max_value').value);
 
+        unsortedArray = generatedArray(numbersAmount, minValue, maxValue);
+        e.preventDefault();
+        document.querySelector('#unsorted-array').textContent = '[' + unsortedArray + ']';
+    });
 
-    if(asc){
-        SortLocal();
-    } else {
-        SortLocal();
+    // 3. Generate an array
+    generatedArray = function (numbers, min, max) {
+        var result = [];
+        for (i = 0; i < numbers; i++) {
+            result.push(Math.floor((Math.random() * (max - min) + min)));
+        }
+        return result;
     }
 
-    function SortLocal(){
-        for(i = 0; i < arr.length; i++) {
-            if(isCompleted == true) {
+    // Sort the generated array
+    document.querySelector('#btn_sort').addEventListener('click', function () {
+        
+        // Select sorting type
+        var sortType = document.querySelector('#select_type').value;
+        var sortedArray = sortBubblesArray(unsortedArray, sortType);
+        // Alert the result
+        var p = document.querySelector('#result > p');
+        
+        p.textContent = '[' + sortedArray + ']';
+        document.querySelector('#result').classList.remove('hidden');
+
+    });
+
+    //1.Bubble sort
+    var sortBubblesArray = function (arr, sorting) {
+        var cur;
+        var prev;
+        var iterationCount = 0;
+        var isCompleted;
+
+        for (i = 0; i < arr.length; i++) {
+            if (isCompleted == true) {
                 break;
             }
             isCompleted = true;
-            for(j=0; j < arr.length; j++) {
+            for (j = 0; j < arr.length; j++) {
                 iterationCount++;
-                if (asc ? arr[j] < arr[j - 1] : arr[j] > arr[j - 1]) {
-                   cur = arr[j];
-                   prev = arr[j-1];
-                   arr[j-1] = cur;
-                   arr[j] = prev;     
-                   isCompleted = false;                        
+                if (sorting === 'ascending' ? arr[j] < arr[j - 1] : arr[j] > arr[j - 1]) {
+                    cur = arr[j];
+                    prev = arr[j - 1];
+                    arr[j - 1] = cur;
+                    arr[j] = prev;
+                    isCompleted = false;
                 }
-            }           
+            }
         }
+        console.log(iterationCount);
+        return arr;
     }
-    console.log(iterationCount);
-    return arr;
-}
-
-
-var example = [1,2,3,4,5,6,7,8,0];
-var result = sortBubblesArray(example, true);
-console.log(result);
+};
